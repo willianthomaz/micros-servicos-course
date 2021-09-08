a702ea4c285310f7b93bd0c69a903e6ce4c6fd2d
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-08 20:39:21 -0300

    Project human-resourses-worker created

---
+++ src/main/java/com/willian/thomaz/humanresourcesworker/HumanResourcesWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresourcesworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/willian/thomaz/humanresourcesworker/HumanResourcesWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresourcesworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
