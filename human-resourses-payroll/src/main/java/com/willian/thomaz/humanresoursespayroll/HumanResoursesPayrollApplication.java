ce9cd6b2431d10f60a17d5ca7b1cf83bb770b53d
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-09 20:51:34 -0300

    Project human-resourses-payroll created

---
+++ human-resourses-payroll/src/main/java/com/willian/thomaz/humanresoursespayroll/HumanResoursesPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresoursespayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResoursesPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResoursesPayrollApplication.class, args);
+    }
+
+}
