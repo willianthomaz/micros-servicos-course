3cdf2b2172fab78e94f14a9000c3233157428060
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-12 14:44:34 -0300

    RestTemplate

--- human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/services/PaymentService.java
+++ human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/services/PaymentService.java
@@ -1,12 +1,29 @@
 package com.willian.thomaz.humanresourcespayroll.services;
 
 import com.willian.thomaz.humanresourcespayroll.entities.Payment;
+import com.willian.thomaz.humanresourcespayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob",200.0, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(),worker.getDailyIncome(), days);
     }
 }
