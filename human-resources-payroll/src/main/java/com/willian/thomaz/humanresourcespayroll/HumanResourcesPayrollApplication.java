1b384be3ee762cbe4dae5a394c4231fc7aa6965e
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-12 19:00:49 -0300

    Ribbon load balancing

--- human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/HumanResourcesPayrollApplication.java
+++ human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/HumanResourcesPayrollApplication.java
@@ -2,9 +2,10 @@ package com.willian.thomaz.humanresourcespayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HumanResourcesPayrollApplication {
--- human-resources-worker/src/main/java/com/willian/thomaz/humanresourcesworker/resources/WorkerResource.java
+++ human-resources-worker/src/main/java/com/willian/thomaz/humanresourcesworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.willian.thomaz.humanresourcesworker.resources;
 
 import com.willian.thomaz.humanresourcesworker.entities.Worker;
 import com.willian.thomaz.humanresourcesworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping("/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +34,9 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
