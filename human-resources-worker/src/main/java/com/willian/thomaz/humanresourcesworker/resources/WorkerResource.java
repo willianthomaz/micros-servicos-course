cc24bafc6d298b48bc3cf8940cce44c3fe83ff44
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-12 13:34:58 -0300

    ajuste

--- human-resources-worker/src/main/java/com/willian/thomaz/humanresourcesworker/resources/WorkerResource.java
+++ human-resources-worker/src/main/java/com/willian/thomaz/humanresourcesworker/resources/WorkerResource.java
@@ -19,13 +19,13 @@ public class WorkerResource {
     private Workerrepository repository;
 
     @GetMapping
-    public ResponseEntity<List<Worker>> findAll(){
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id){
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
