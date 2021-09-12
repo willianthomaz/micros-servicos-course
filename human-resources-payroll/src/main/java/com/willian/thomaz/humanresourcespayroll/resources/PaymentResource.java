e326e9b9d5986ffe911496d8f5ec41ac402b33d9
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-12 14:09:04 -0300

    hr-payroll implementation mock

--- human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/resources/PaymentResource.java
+++ human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/resources/PaymentResource.java
@@ -1,2 +1,24 @@
-package com.willian.thomaz.humanresourcespayroll.resources;public class PaymentResource {
+package com.willian.thomaz.humanresourcespayroll.resources;
+
+import com.willian.thomaz.humanresourcespayroll.entities.Payment;
+import com.willian.thomaz.humanresourcespayroll.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping("/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
 }
--- human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/services/PaymentService.java
+++ human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/services/PaymentService.java
@@ -1,2 +1,12 @@
-package com.willian.thomaz.humanresourcespayroll.services;public class PaymentService {
+package com.willian.thomaz.humanresourcespayroll.services;
+
+import com.willian.thomaz.humanresourcespayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Bob",200.0, days);
+    }
 }
