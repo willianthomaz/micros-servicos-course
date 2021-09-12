2d986fc77322ca8b74d29db5956c37ed646de962
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-12 13:33:10 -0300

    ajuste diretorio

---
+++ human-resources-payroll/src/main/java/com/willian/thomaz/humanresourcespayroll/HumanResourcesPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresourcespayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesPayrollApplication.class, args);
+    }
+
+}
---
+++ human-resources-payroll/src/test/java/com/willian/thomaz/humanresourcespayroll/HumanResourcesPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresourcespayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
