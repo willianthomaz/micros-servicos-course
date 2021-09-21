700ca3477a972afb388ad36bece956cc58cf373c
Willian Thomaz <willian_thomaz@outlook.com>
2021-09-21 20:09:15 -0300

    Project hr-eureka-server created

---
+++ human-resources-eureka-server/src/main/java/com/willian/thomaz/humanresourceseurekaserver/HumanResourcesEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresourceseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanResourcesEurekaServerApplication.class, args);
+    }
+
+}
---
+++ human-resources-eureka-server/src/test/java/com/willian/thomaz/humanresourceseurekaserver/HumanResourcesEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.humanresourceseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
